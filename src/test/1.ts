b7113cde4079bb8bf14454be4f53958496672f27
jeff <jeff-hui@qq.com>
2024-01-15 00:16:30 +0800

    fix

--- src/test/1.ts
+++
@@ -1,3 +0,0 @@
-import { SearchApi } from '@/Api/search'
-
-console.log(await SearchApi.SearchSuggest1('as'))
--- src/test/1.vue
+++ src/test/1.vue
@@ -1,26 +1,5 @@
-<!--
- * @Author: GAO GAO
- * @Date: 2023-10-12 17:45:15
--->
-<template>
-  <div ref="s">1234</div>
-</template>
+<script setup lang="ts"></script>
 
-<script setup lang="ts">
-function a(dom) {
-  this.dom = dom
-}
-
-a.prototype.b = function () {
-  console.log('b')
-}
-let s = ref(null)
-
-onMounted(async () => {
-  await nextTick()
-  let aa = new a(s.value)
-  aa.b()
-})
-</script>
+<template></template>
 
 <style scoped lang="scss"></style>
